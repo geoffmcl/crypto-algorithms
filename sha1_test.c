b9ffe90d9b01095bac604ddac94c974d79422732
Geoff McLane <ubuntu@geoffair.info>
2017-11-25 19:07:39 +0100

    Enhance sha1_test

--- sha1_test.c
+++ sha1_test.c
@@ -13,8 +13,10 @@
 
 /*************************** HEADER FILES ***************************/
 #include <stdio.h>
+#include <stdlib.h> /* for malloc, ... */
 #include <memory.h>
 #include <string.h>
+#include "cry_utils.h"
 #include "sha1.h"
 
 /*********************** FUNCTION DEFINITIONS ***********************/
@@ -50,9 +52,174 @@ int sha1_test()
 	return(pass);
 }
 
-int main()
+int test_main()
 {
 	printf("SHA1 tests: %s\n", sha1_test() ? "SUCCEEDED" : "FAILED");
 
 	return(0);
 }
+
+static const char *module = "sha1_test";
+
+static const char *usr_input = 0;
+static int out_Upper = 0;
+static BYTE check[SHA1_BLOCK_SIZE];
+static int got_Check = 0;
+
+void give_help(char *name)
+{
+    printf("%s: usage: [options] usr_input\n", module);
+    printf("Options:\n");
+    printf(" --help  (-h or -?) = This help and exit(0)\n");
+    printf(" --upper       (-u) = Output string in uppercase.\n");
+    printf(" --check <hex> (-c) = Compare results to this hex string.\n");
+    printf(" --test        (-t) = Run the original 'tests' and exit(0)\n");
+    printf("\n");
+    printf(" Print BSD-style SHA1 checksum for the 'input' file.\n");
+    // TODO: More help
+}
+
+int parse_args(int argc, char **argv)
+{
+    int i, i2, c;
+    char *arg, *sarg;
+    size_t len, ii;
+    for (i = 1; i < argc; i++) {
+        arg = argv[i];
+        i2 = i + 1;
+        if (*arg == '-') {
+            sarg = &arg[1];
+            while (*sarg == '-')
+                sarg++;
+            c = *sarg;
+            switch (c) {
+            case 'h':
+            case '?':
+                give_help(argv[0]);
+                return 2;
+                break;
+            case 'u':
+                out_Upper = 1;
+                break;
+            case 'c':
+                if (i2 < argc) {
+                    i++;
+                    sarg = argv[i];
+                    len = strlen(sarg);
+                    if (len != (SHA1_BLOCK_SIZE * 2)) {
+                        printf("%s: Error: Expected SHA256 hex checksum of length %u, not %u\n", module,
+                            (int)(SHA1_BLOCK_SIZE * 2), (int)len);
+                        return 1;
+                    }
+                    for (ii = 0, len = 0; ii < SHA1_BLOCK_SIZE; ii++, len += 2) {
+                        c = hex_to_val(sarg[len]);
+                        if (c == -1) {
+                            printf("%s: Error: SHA256 checksum contains invalid value %u\n", module, c);
+                            return 1;
+                        }
+                        check[ii] = c * 16;
+                        c = hex_to_val(sarg[len + 1]);
+                        if (c == -1) {
+                            printf("%s: Error: SHA256 checksum contains invalid value %u\n", module, c);
+                            return 1;
+                        }
+                        check[ii] += c;
+                    }
+                    got_Check = 1;
+                }
+                else {
+                    printf("%s: Error: Expected SHA1 hex checksum to follow '%s'\n", module, arg);
+                    return 1;
+                }
+
+                break;
+            case 't':
+                test_main();
+                return 2;
+                break;
+                // TODO: Other arguments
+            default:
+                printf("%s: Unknown argument '%s'. Try -? for help...\n", module, arg);
+                return 1;
+            }
+        }
+        else {
+            // bear argument
+            if (usr_input) {
+                printf("%s: Already have input '%s'! What is this '%s'?\n", module, usr_input, arg);
+                return 1;
+            }
+            usr_input = strdup(arg);
+        }
+    }
+    if (!usr_input) {
+        printf("%s: No user input found in command!\n", module);
+        return 1;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    int iret, i, c;
+    size_t len, sz;
+    BYTE *fbuf;
+    FILE *fp;
+    BYTE buf[SHA1_BLOCK_SIZE];
+    SHA1_CTX ctx;
+    const char *form = "%02x";
+    iret = parse_args(argc, argv);
+    if (iret) {
+        if (iret == 2)
+            iret = 0;
+        return iret;
+    }
+    const char *file = usr_input;
+    if (is_file_or_directory(file) != MDT_FILE) {
+        printf("Error: Can NOT stat file '%s'\n", file);
+        return 1;
+    }
+    len = get_last_file_size();
+    fp = fopen(file, "rb");
+    if (!fp) {
+        printf("Error: Can NOT open file '%s'\n", file);
+        return 1;
+    }
+    fbuf = (BYTE *)malloc(len + 1);
+    if (!fbuf) {
+        fclose(fp);
+        printf("Error: Memory failed on %u bytes\n", (int)len);
+        return 1;
+    }
+    sz = fread(fbuf, 1, len, fp);
+    fclose(fp);
+    if (sz != len) {
+        free(fbuf);
+        printf("Error: Read of file '%s' failed!\n", file);
+        return 1;
+    }
+    sha1_init(&ctx);
+    sha1_update(&ctx, fbuf, len);
+    sha1_final(&ctx, buf);
+    free(fbuf);
+    printf("SHA1 (%s) = ", file);
+    if (out_Upper)
+        form = "%02X";
+    for (i = 0; i < SHA1_BLOCK_SIZE; i++) {
+        c = buf[i] & 0xff;
+        printf(form, c);
+    }
+    if (got_Check) {
+        if (memcmp(check, buf, SHA1_BLOCK_SIZE)) {
+            printf(" check FAILED!");
+            iret = 1;
+        }
+        else {
+            printf(" Valid");
+        }
+    }
+    printf("\n");
+    return iret;
+}
+
+/* eof - sha1_test.c */

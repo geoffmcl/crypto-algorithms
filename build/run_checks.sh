e2028449484242ad6c25cc2dac055050c8c3b732
Geoff rpi McLane <ubuntu@geoffair.info>
2017-11-27 19:34:53 +0000

    Issue #1 - Update run_checks.sh to use '--out fil' opt

--- build/run_checks.sh
+++ build/run_checks.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+
 TMPFIL="bldlog-1.txt"
 if [ ! -f "$TMPFIL" ]; then
     echo "Can NOT locate '$TMPFIL'! *** FIX ME ***"
     echo "This can be ANY existing file..."
     exit 1
 fi
+
 TMPCHK="temp-$TMPFIL"
 
 TMPEXE="md5_test"
@@ -16,9 +18,10 @@ TMPOUT="$TMPCHK.MD5"
 if [ -f "$TMPOUT" ]; then
     rm -f $TMPOUT
 fi
-echo "Doing: '$TMPEXE $TMPFIL' to $TMPOUT"
-./$TMPEXE $TMPFIL > $TMPOUT
+echo "Doing: '$TMPEXE $TMPFIL -o $TMPOUT'"
+./$TMPEXE $TMPFIL -o $TMPOUT
 cat $TMPOUT
+echo ""
 echo "Doing: '$TMPEXE -c $TMPOUT'"
 ./$TMPEXE -c $TMPOUT
 echo ""
@@ -28,14 +31,15 @@ TMPEXE="sha1_test"
 if [ ! -x "$TMPEXE" ]; then
     echo "Error: Can NOT locate $TMPEXE! *** FIX ME ***"
     exit 1
-fi    
+fi
 TMPOUT="$TMPCHK.SHA1"
 if [ -f "$TMPOUT" ]; then
     rm -f $TMPOUT
 fi
-echo "Doing: '$TMPEXE $TMPFIL' to $TMPOUT"
-./$TMPEXE $TMPFIL > $TMPOUT
+echo "Doing: '$TMPEXE $TMPFIL -o $TMPOUT'"
+./$TMPEXE $TMPFIL -o $TMPOUT
 cat $TMPOUT
+echo ""
 echo "Doing: '$TMPEXE -c $TMPOUT'"
 ./$TMPEXE -c $TMPOUT
 echo ""
@@ -44,17 +48,17 @@ TMPEXE="sha256_test"
 if [ ! -x "$TMPEXE" ]; then
     echo "Error: Can NOT locate $TMPEXE! *** FIX ME ***"
     exit 1
-fi    
+fi
 TMPOUT="$TMPCHK.SHA256"
 if [ -f "$TMPOUT" ]; then
     rm -f $TMPOUT
 fi
-echo "Doing: '$TMPEXE $TMPFIL' to $TMPOUT"
-./$TMPEXE $TMPFIL > $TMPOUT
+echo "Doing: '$TMPEXE $TMPFIL -o $TMPOUT'"
+./$TMPEXE $TMPFIL -o $TMPOUT
 cat $TMPOUT
+echo ""
 echo "Doing: '$TMPEXE -c $TMPOUT'"
 ./$TMPEXE -c $TMPOUT
 echo ""
 
 # eof
-

08325f95f05141ef3a46c759983c7c41ba9f66ce
Geoff R. McLane <ubuntu@geoffair.info>
2017-12-02 20:22:54 +0100

    Check exit code when -Test run

--- build/run_tests.sh
+++ build/run_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+TMPFAIL=""
 TMPOPTS="-T"
+TMPTEST="md5"
 TMPEXE="md5_test"
 if [ ! -x "$TMPEXE" ]; then
     echo "Error: Can NOT locate $TMPEXE! *** FIX ME ***"
@@ -7,7 +9,11 @@ if [ ! -x "$TMPEXE" ]; then
 fi    
 echo "Doing: $TMPEXE $TMPOPTS"
 ./$TMPEXE $TMPOPTS
+if [ ! "$?" = "0" ]; then
+    TMPFAIL="$TMPFAIL $TMPTEST"
+fi
 
+TMPTEST="sha1"
 TMPEXE="sha1_test"
 if [ ! -x "$TMPEXE" ]; then
     echo "Error: Can NOT locate $TMPEXE! *** FIX ME ***"
@@ -15,7 +21,11 @@ if [ ! -x "$TMPEXE" ]; then
 fi    
 echo "Doing: $TMPEXE $TMPOPTS"
 ./$TMPEXE $TMPOPTS
+if [ ! "$?" = "0" ]; then
+    TMPFAIL="$TMPFAIL $TMPTEST"
+fi
 
+TMPTEST="sha256"
 TMPEXE="sha256_test"
 if [ ! -x "$TMPEXE" ]; then
     echo "Error: Can NOT locate $TMPEXE! *** FIX ME ***"
@@ -23,6 +33,17 @@ if [ ! -x "$TMPEXE" ]; then
 fi    
 echo "Doing: $TMPEXE $TMPOPTS"
 ./$TMPEXE $TMPOPTS
+if [ ! "$?" = "0" ]; then
+    TMPFAIL="$TMPFAIL $TMPTEST"
+fi
+
+echo ""
+if [ -z "$TMPFAIL" ]; then
+	echo "Appears ALL tests PASSED"
+else
+	echo "Tests $TMPFAIL FAILED"
+fi
+echo ""
 
 # eof
 

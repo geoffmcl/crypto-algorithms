39b7227b6cec99ba1717ab7dd905a8be1c22025b
Geoff R. McLane <ubuntu@geoffair.info>
2017-12-02 19:55:21 +0100

    Show FAILED tests...

--- build/run_tests2.sh
+++ build/run_tests2.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 BN=`basename $0`
 
+TMPFAIL=""
 TMPLIST="aes_test arcfour_test base64_test blowfish_test des_test md2_test rot-13_test"
 
 for var in $TMPLIST; do
     if [ -x "$var" ]; then
         echo "$BN: Doing './$var'"
         ./$var
+        if [ ! "$?" = "0" ]; then
+            TMPFAIL="$TMPFAIL $var"
+        fi
     else
         echo "$BN: Exe './$var' not found."
     fi
 done 
+echo ""
+if [ -z "$TMPFAIL" ]; then
+    echo "$BN: Appears ALL tests PASSED"
+else
+    echo "$BN: Tests $TMPFAIL FAILED"
+fi
+echo ""
 
 # eof
 

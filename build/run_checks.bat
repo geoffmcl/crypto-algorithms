10e9bc652de01d17a4c8a3c4193476f14de4b350
Geoff McLane <ubuntu@geoffair.info>
2017-12-03 02:05:58 +0100

    Test errorlevel in run_checks.bat

--- build/run_checks.bat
+++ build/run_checks.bat
@@ -1,6 +1,8 @@
 @setlocal
+@set TMPFAIL=
 @set TMPFIL=bldlog-1.txt
 @set TMPCHK=temp-%TMPFIL%
+@echo.
 
 @set TMPEXE=Release\md5_test.exe
 @if NOT EXIST %TMPEXE% goto NOEXE
@@ -9,8 +11,14 @@
 @echo Doing: %TMPEXE% %TMPFIL% -o %TMPOUT%
 @%TMPEXE% %TMPFIL% -o %TMPOUT%
 @type %TMPOUT%
+@echo.
 @echo Doing: %TMPEXE% -c %TMPOUT%
 @%TMPEXE% -c %TMPOUT%
+@if ERRORLEVEL 1 goto FAILED1
+@goto DNONE1
+:FAILED1
+@set TMPFAIL=%TMPFAIL% MD5
+:DNONE1
 @echo.
 
 @set TMPEXE=Release\sha1_test.exe
@@ -20,8 +28,14 @@
 @echo Doing: %TMPEXE% %TMPFIL% -o %TMPOUT%
 @%TMPEXE% %TMPFIL% -o %TMPOUT%
 @type %TMPOUT%
+@echo.
 @echo Doing: %TMPEXE% -c %TMPOUT%
 @%TMPEXE% -c %TMPOUT%
+@if ERRORLEVEL 1 goto FAILED2
+@goto DNONE2
+:FAILED2
+@set TMPFAIL=%TMPFAIL% SHA1
+:DNONE2
 @echo.
 
 @set TMPEXE=Release\sha256_test.exe
@@ -31,8 +45,21 @@
 @echo Doing: %TMPEXE% %TMPFIL% -o %TMPOUT%
 @%TMPEXE% %TMPFIL% -o %TMPOUT%
 @type %TMPOUT%
+@echo.
 @echo Doing: %TMPEXE% -c %TMPOUT%
 @%TMPEXE% -c %TMPOUT%
+@if ERRORLEVEL 1 goto FAILED3
+@goto DNONE3
+:FAILED3
+@set TMPFAIL=%TMPFAIL% SHA256
+:DNONE3
+@echo.
+
+@if "%TMPFAIL%x" == "x" (
+@echo Appears ALL 'checks' PASSED
+) else (
+@echo 'check' %TMPFAIL% FAILED
+)
 @echo.
 
 @goto END
